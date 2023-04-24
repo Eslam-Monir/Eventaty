02f6ddcc99aa16c6289599445d95bf3c7b29f2f1
Eslam-Monir <100922244+Eslam-Monir@users.noreply.github.com>
2023-04-24 23:22:41 +0200

    pushing connection

---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,42 @@
+﻿using Oracle.DataAccess.Client;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+
+        string ordb = "Data source=orcl;User Id=scott;Password=tiger;";
+        OracleConnection conn;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new OracleConnection(ordb);
+            conn.Open();
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "select JOB from EMP";
+            cmd.CommandType = CommandType.Text;
+            OracleDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                comboBox1.Items.Add(dr[0]);
+            }
+            dr.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
