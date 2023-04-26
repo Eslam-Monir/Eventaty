d27220430ba6bc51bbc9148732d6a070dd266aeb
Eslam-Monir <100922244+Eslam-Monir@users.noreply.github.com>
2023-04-26 04:11:41 +0200

    Finished the Login form Added User home form Added PlaceOwner home form

---
+++ WindowsFormsApp5/LoginForm.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Login_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userName = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Verify = new System.Windows.Forms.Label();
+            this.IsPO = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // userName
+            // 
+            this.userName.Location = new System.Drawing.Point(161, 184);
+            this.userName.Name = "userName";
+            this.userName.Size = new System.Drawing.Size(189, 20);
+            this.userName.TabIndex = 0;
+            this.userName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Password
+            // 
+            this.Password.Location = new System.Drawing.Point(161, 224);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(189, 20);
+            this.Password.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(158, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(158, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(218, 284);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Verify
+            // 
+            this.Verify.AutoSize = true;
+            this.Verify.Location = new System.Drawing.Point(211, 325);
+            this.Verify.Name = "Verify";
+            this.Verify.Size = new System.Drawing.Size(0, 13);
+            this.Verify.TabIndex = 5;
+            this.Verify.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // IsPO
+            // 
+            this.IsPO.AutoSize = true;
+            this.IsPO.Location = new System.Drawing.Point(161, 250);
+            this.IsPO.Name = "IsPO";
+            this.IsPO.Size = new System.Drawing.Size(107, 17);
+            this.IsPO.TabIndex = 6;
+            this.IsPO.Text = "I am place owner";
+            this.IsPO.UseVisualStyleBackColor = true;
+            // 
+            // Login_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 450);
+            this.Controls.Add(this.IsPO);
+            this.Controls.Add(this.Verify);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.userName);
+            this.Name = "Login_form";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox userName;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Verify;
+        private System.Windows.Forms.CheckBox IsPO;
+    }
+}
---
+++ WindowsFormsApp5/LoginForm.cs
@@ -0,0 +1,98 @@
+﻿using Oracle.DataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+
+
+
+namespace WindowsFormsApp5
+{
+    public partial class Login_form : Form
+    {
+        string ordb = "Data source=orcl;User Id=scott;Password=tiger;";
+        OracleConnection conn;
+
+        public Login_form()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            conn = new OracleConnection(ordb);
+            conn.Open();
+            OracleCommand cmd = new OracleCommand();
+          
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+         
+        {   conn = new OracleConnection(ordb);
+            conn.Open();
+            OracleCommand cmd = new OracleCommand();
+
+
+            cmd.Connection = conn;
+            if (!IsPO.Checked) {
+                cmd.CommandText = "select * from Users where Name = :name and Password= :password";
+            }
+            else
+            {
+                cmd.CommandText = "select * from placeowner where Name = :name and Password= :password";
+            }
+            cmd.Parameters.Add("name", userName.Text);
+            cmd.Parameters.Add("password", Password.Text);
+            cmd.CommandType = CommandType.Text;
+            OracleDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                Verify.Text = "Login Successfull ";
+                if (!IsPO.Checked)
+                {
+                    this.Hide();
+                    UserHome HomeForm = new UserHome();
+                    HomeForm.Show();
+                  
+                }
+                else
+                {
+                    this.Hide();
+                    PlaceOwnerHome HomeForm = new PlaceOwnerHome();
+                    HomeForm.Show();
+                }
+            }
+            else { Verify.Text = "Login Unsuccessfull "; }
+            dr.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
