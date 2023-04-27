fb640a77ac499bfca7dcad2c452ee5dde9c270aa
Eslam-Monir <100922244+Eslam-Monir@users.noreply.github.com>
2023-04-27 17:44:32 +0200

    mad a list of events

--- WindowsFormsApp5/PlaceOwnerHome.cs
+++ WindowsFormsApp5/PlaceOwnerHome.cs
@@ -1,6 +1,7 @@
 ﻿using Oracle.DataAccess.Client;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -19,6 +20,9 @@ namespace WindowsFormsApp5
         {
             InitializeComponent();
         }
+       /* KeyValuePair<int, string> PoEvents = new KeyValuePair<int, string>();*/
+        Dictionary<int, string> PoEvents = new Dictionary<int, string>();
+       
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -26,19 +30,44 @@ namespace WindowsFormsApp5
             conn.Open();
             OracleCommand cmd = new OracleCommand();
             cmd.Connection = conn;
-            cmd.CommandText = "  select * from   events inner  join place on  Events.location = place.id where  place.po_id= "+Login_form.ID;
-            Console.WriteLine(cmd.CommandText);
-            
-            
+            cmd.CommandText = "  select * from events inner join place on  Events.location = place.id where  place.po_id= "+Login_form.ID;
+           
+    
+             List<Events> PownerEvents= new List<Events>(); 
+
             /**//*and Place.PO_ID= "+Login_form.ID;*/
             cmd.CommandType = CommandType.Text;
             OracleDataReader dr = cmd.ExecuteReader();
             while (dr.Read())
             {
-                listView1.Items.Add(dr[1].ToString());
-                    
-            }
+                /*listView1.Items.Add(dr[1].ToString());*/
+                /*  PoEvents.Add(Int32.Parse(dr[0].ToString()), dr[1].ToString());*/
 
+                Events pEvent = new Events(
+               id:Int32.Parse(dr[0].ToString()),
+                     name:   dr[1].ToString(),
+                date:    dr[2].ToString(),
+                 time:   dr[3].ToString(),
+                 attendee_limit: Int32.Parse(dr[4].ToString()) ,
+                  description: dr[5].ToString(), 
+                   categories: dr[6].ToString(),
+                   location: dr[7].ToString(),
+                   status: Int32.Parse(dr[8].ToString())
+
+                    );
+
+                
+                PownerEvents.Add( pEvent );
+
+            }
+            /*      for (int i = 0;i<PownerEvents.Count;i++)
+                  {
+                      listView1.Items.Add(PoEvents.Values.);
+                  }*/
+            foreach (Events evnt in PownerEvents)
+            {
+                listView1.Items.Add(evnt.name);
+            }
         }
 
 
@@ -53,6 +82,21 @@ namespace WindowsFormsApp5
         private void label1_Click(object sender, EventArgs e)
         {
   
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
